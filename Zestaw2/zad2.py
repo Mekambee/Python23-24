ca832df1b4c56da3a6ec5161db487d8b7b5b21ed
Mekambee <kamil.piechotaaax@gmail.com>
2024-10-28 21:25:53 +0100

    z2_zad2_pop

--- Zestaw2/zad2.py
+++ Zestaw2/zad2.py
@@ -1,10 +1,24 @@
 import re
+from collections import Counter
 def stringCounter(string):
-    words_count = len(re.findall(r'\b[a-zA-Z0-9]+\b',string))
-    letters_count = len(re.findall(r'[a-zA-Z]', string))
-    digits_count = len(re.findall(r'[0-9]', string))
-    print(f"Dła słowa[ {string} ]: \n Liczba słów: {words_count} \n Liczba liter: {letters_count} \n Liczba cyfr: {digits_count}")
+    words = re.findall(r'\b[a-zA-Z0-9]+\b',string)
+    letters = re.findall(r'[a-zA-Z]', string)
+    digits = re.findall(r'[0-9]', string)
+    print(f"Dła słowa: {string}  \n Liczba słów: {len(words)} \n Liczba liter: {len(letters)} \n Liczba cyfr: {len(digits)}")
 
+    print("Częstość występowania poszczególnych liter:")
+
+    letter_frequency = Counter(letter.lower() for letter in letters)
+    sorted(letter_frequency)
+    for letter, count in letter_frequency.items():
+        print(f"{letter}: {count}")
+
+    print("Częstość występowania poszczególnych cyfr:")
+
+    digit_frequency = Counter(digit for digit in digits)
+    sorted(digit_frequency)
+    for digit, count in digit_frequency.items():
+        print(f"{digit}: {count}")
 
 if __name__ == "__main__":
     test_word = "Ala a b ma kota 4 5 6 $$$$$ #"
